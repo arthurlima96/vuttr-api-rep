ed7bc61c77114638d0d46442e758ac6d14557d85
arthur lima <arthur@172.20.80.74>
2020-07-20 12:57:29 -0300

    Chamada do sequence na inserção dos dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-insert into tools (id,title,link,description) values (1,'Notion','https://notion.so','All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized.');
+insert into tools (id,title,link,description) values (HIBERNATE_SEQUENCE.nextval,'Notion','https://notion.so','All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized.');
 insert into tools_tags(tools_id,tag) values (1,'organization');
 insert into tools_tags(tools_id,tag) values (1,'planning');
 insert into tools_tags(tools_id,tag) values (1,'collaboration');
 insert into tools_tags(tools_id,tag) values (1,'writing');
 insert into tools_tags(tools_id,tag) values (1,'calendar');
 
-insert into tools (id,title,link,description) values (2,'json-server','https://github.com/typicode/json-server','Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.');
+insert into tools (id,title,link,description) values (HIBERNATE_SEQUENCE.nextval,'json-server','https://github.com/typicode/json-server','Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.');
 insert into tools_tags(tools_id,tag) values (2,'api');
 insert into tools_tags(tools_id,tag) values (2,'json');
 insert into tools_tags(tools_id,tag) values (2,'schema');
@@ -14,7 +14,7 @@ insert into tools_tags(tools_id,tag) values (2,'github');
 insert into tools_tags(tools_id,tag) values (2,'rest');
 
 
-insert into tools (id,title,link,description) values (3,'fastify','https://www.fastify.io/','Extremely fast and simple, low-overhead web framework for NodeJS. Supports HTTP2.');
+insert into tools (id,title,link,description) values (HIBERNATE_SEQUENCE.nextval,'fastify','https://www.fastify.io/','Extremely fast and simple, low-overhead web framework for NodeJS. Supports HTTP2.');
 insert into tools_tags(tools_id,tag) values (3,'web');
 insert into tools_tags(tools_id,tag) values (3,'framework');
 insert into tools_tags(tools_id,tag) values (3,'node');
