603061c3a9f5dd79fcaa7941ed2572a60c2449c7
arthur lima <arthur@172.20.80.74>
2020-07-20 12:58:08 -0300

    Criação da endpoint para excluir uma ferramenta

--- src/main/java/br/com/startaideia/vuttr/controller/ToolsController.java
+++ src/main/java/br/com/startaideia/vuttr/controller/ToolsController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +39,16 @@ public class ToolsController {
 		consumes = MediaType.APPLICATION_JSON_VALUE
 	)
 	public ResponseEntity<Tools> salvarFerramentas(@RequestBody ToolsRequest toolsRequest) {
-		return new ResponseEntity<>(toolsRepository.save(toolsRequest.novaFerramenta()),HttpStatus.CREATED);
+		return new ResponseEntity<>(toolsRepository.save(toolsRequest.novaFerramenta()), HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<?> deletarFerramenta(@PathVariable ("id") Long id) {
+		if (toolsRepository.findById(id).isPresent()) {
+			toolsRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<>(id, HttpStatus.BAD_REQUEST);
+		}
 	}
 }
