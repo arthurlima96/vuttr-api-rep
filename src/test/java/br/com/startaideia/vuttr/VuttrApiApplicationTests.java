5b684862ccca7bb357649698178fe9976c9fe763
arthur lima <arthur@172.20.80.74>
2020-07-20 00:22:28 -0300

    Adição dos arquivos iniciais do projeto

---
+++ src/test/java/br/com/startaideia/vuttr/VuttrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.startaideia.vuttr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuttrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
